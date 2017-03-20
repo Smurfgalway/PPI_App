500dc1ff5552fd22017fc892b6379f1e88655e46
smurgalway <g00323639@gmit.ie>
2017-03-20 16:37:17 +0000

    map

--- myApp/src/pages/Hub2/sHub.ts
+++ myApp/src/pages/Hub2/sHub.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
 
 import { NavController } from 'ionic-angular';
 
@@ -8,8 +8,25 @@ import { NavController } from 'ionic-angular';
 })
 export class sHubPage {
 
+  @ViewChild('map') mapElement; 
+  map: any;
   constructor(public navCtrl: NavController) {
 
   }
+  ionViewDidLoad(){
+    this.initmap();
+  }
 
+  
+  initmap(){
+    let latlng = new google.maps.latLng(Geolocation);
+
+    let mapOptions = {
+      center: latlng,
+      zoom: 15,
+      mapTypeId: google.maps.MapTypeId.ROADMAP
+    };
+  
+    this.map = new google.maps.Map(this.mapElement.nativeElement, mapOptions);
+  }
 }
