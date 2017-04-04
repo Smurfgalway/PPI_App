fa5a1cd19007fea840cbd7ab9817859488f5ff01
smurgalway <g00323639@gmit.ie>
2017-04-04 12:41:10 +0100

    marker adding and geolocation

--- myApp/src/pages/Hub2/sHub.ts
+++ myApp/src/pages/Hub2/sHub.ts
@@ -1,6 +1,6 @@
 import { Component, ViewChild } from '@angular/core';
-
 import { NavController } from 'ionic-angular';
+import { Geolocation} from 'ionic-native';
 
 declare var google;
 
@@ -21,14 +21,44 @@ export class sHubPage {
 
   
   initmap(){
-    let latlng = new google.maps.LatLng(53.278640, -9.010414);
+    
+    Geolocation.getCurrentPosition().then((position) => {
+ 
+      let latLng = new google.maps.LatLng(position.coords.latitude, position.coords.longitude);
 
     let mapOptions = {
-      center: latlng,
-      zoom: 15,
+      center: latLng,
+      zoom: 17,
       mapTypeId: google.maps.MapTypeId.ROADMAP
-    };
+    }
+   
+    
   
     this.map = new google.maps.Map(this.mapElement.nativeElement, mapOptions);
+    });
   }
+  addMarker(){
+ 
+  let marker = new google.maps.Marker({
+    map: this.map,
+    animation: google.maps.Animation.DROP,
+    position: this.map.getCenter()
+  });
+ 
+  let content = "dropping a marker";          
+ 
+  this.addInfoWindow(marker, content);
+ 
+}
+addInfoWindow(marker, content){
+ 
+  let infoWindow = new google.maps.InfoWindow({
+    content: content
+  });
+ 
+  google.maps.event.addListener(marker, 'click', () => {
+    infoWindow.open(this.map, marker);
+  });
+ 
+}
 }
