4b112042d4b53dae6261fcb276d848f2f44ac829
smurgalway <g00323639@gmit.ie>
2017-04-23 00:24:43 +0100

    routes and navigation functionality added

--- Main.py
+++ Main.py
@@ -3,10 +3,22 @@ from flask import render_template
 
 app = fl.Flask(__name__)
 
+#Loads the Index html page as my render template loading on my server 
 @app.route("/")
 def name():
     return render_template('Index.html')
+#set up for my button navigation for my map loading
+@app.route("/map")
+def map():
+    return render_template('Main.html')
 
+@app.route("/mental")
+def mental():
+    return render_template('Mental.html')
+
+@app.route("/sh")
+def sh():
+    return render_template('sexual.html')
 
 if __name__ == "__main__":
     app.run()
