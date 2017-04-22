88a35750f80018aa9993f2674d045a4a4db2732a
smurgalway <g00323639@gmit.ie>
2017-04-22 17:19:04 +0100

    python file updated

--- Main.py
+++ Main.py
@@ -1,10 +1,12 @@
 import flask as fl
 from flask import render_template
+
 app = fl.Flask(__name__)
 
 @app.route("/")
 def name():
-    return render_template('Main.html')
+    return render_template('Index.html')
+
 
 if __name__ == "__main__":
     app.run()
