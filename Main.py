bf86a76ca7e4c4ea1dcc7e76ad87ed07311f4ceb
smurgalway <g00323639@gmit.ie>
2017-05-17 20:46:33 +0100

    updates for database interaction

--- Main.py
+++ Main.py
@@ -1,7 +1,42 @@
 import flask as fl
-from flask import render_template # imports the render template property which allows html pages be loaded as the template for the pages on the server
+import sqlite3
+from flask import render_template, g, request# imports the render template property which allows html pages be loaded as the template for the pages on the server
 
+DATABASE = 'data/thisdata.db'
+con = sqlite3.connect(DATABASE)
+c= con.cursor()
 app = fl.Flask(__name__)
+
+#opens the database
+def pullData():
+    db = getattr(fl.g, '_database', None)
+    if db is None:
+        db = fl.g._database = sqlite3.connect(DATABASE)
+    return db
+
+#closes the database
+@app.teardown_appcontext
+def close_connection(exception):
+    db = getattr(fl.g, '_database', None)
+    if db is not None:
+        db.close()
+
+#inserts data into the database
+#-------------------------------
+@app.route("/data", methods=["POST"])
+def database():
+    c.execute('INSERT INTO UserReviews(User, Review) VALUES(?,?)',(fl.request.form['User'],fl.request.form['Review']))
+    con.commit()
+    return (render_template('review.html') + str(c.fetchall()) )
+
+
+#shows the contents of the database
+#-------------------------------
+@app.route("/show", methods=[ "GET"])
+def showed():
+    c.execute("Select * from UserReviews")
+    thisdata = [dict(User=row[0], Review=row[1]) for row in c.fetchall()]
+    return (render_template('review.html', thisdata=thisdata) + str(c.fetchall()) )
 
 #Loads the Index html page as my render template loading on my server 
 @app.route("/")
@@ -35,6 +70,10 @@ def mental():
 @app.route("/sh")
 def sh():
     return render_template('sexual.html')
+
+@app.route("/re")
+def re():
+    return render_template('review.html')
 
 if __name__ == "__main__":
     app.run() # runs the app
